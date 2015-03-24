00626aa9ce873479dda0698cc2ffd7330f1890c7
Yosuke ONOUE <onoue@likr-lab.com>
2015-03-25 02:03:28 +0900

    Add dcopy

--- index.js
+++ index.js
@@ -45,9 +45,31 @@ function LinalgModule(stdlib, foreign, heap) {
     return value;
   }
 
+  function dcopy(n, x, incX, y, incY) {
+    n = n | 0;
+    x = x | 0;
+    incX = incX | 0;
+    y = y | 0;
+    incY = incY | 0;
+
+    var i = 0,
+        j = 0;
+
+    n = n << 3;
+    x = x << 3;
+    incX = incX << 3;
+    y = y << 3;
+    incY = incY << 3;
+
+    for (i = x, j = y; (i | 0) < (n | 0); i = i + incX | 0, j = j + incY | 0) {
+      darray[j >> 3] = darray[i >> 3];
+    }
+  }
+
   return {
     daxpy: daxpy,
-    dasum: dasum
+    dasum: dasum,
+    dcopy: dcopy
   };
 }
 
--- test.js
+++ test.js
@@ -6,8 +6,8 @@ describe('daxpy', function() {
   it('calculate y := a x + y', function() {
     var heap = new ArrayBuffer(64),
         x = new Float64Array(heap, 0, 4),
-        y = new Float64Array(heap, 32, 4);
-    var linalg = LinalgModule(global, null, heap);
+        y = new Float64Array(heap, 32, 4),
+        linalg = LinalgModule(global, null, heap);
 
     x[0] = 1;
     x[1] = 2;
@@ -30,8 +30,8 @@ describe('daxpy', function() {
 describe('dasum', function() {
   it('return |x_1| + ... + |x_n|', function() {
     var heap = new ArrayBuffer(32),
-        x = new Float64Array(heap, 0, 4);
-    var linalg = LinalgModule(global, null, heap);
+        x = new Float64Array(heap, 0, 4),
+        linalg = LinalgModule(global, null, heap);
 
     x[0] = 0.5;
     x[1] = -1.5;
@@ -41,5 +41,26 @@ describe('dasum', function() {
     var result = linalg.dasum(4, 0, 1);
 
     expect(result).to.be(8);
+  });
+});
+
+describe('dcopy', function() {
+  it('calculate y := x', function() {
+    var heap = new ArrayBuffer(64),
+        x = new Float64Array(heap, 0, 4),
+        y = new Float64Array(heap, 32, 4),
+        linalg = LinalgModule(global, null, heap);
+
+    x[0] = 1;
+    x[1] = 2;
+    x[2] = 3;
+    x[3] = 4;
+
+    linalg.dcopy(4, 0, 1, 4, 1);
+
+    expect(y[0]).to.be(1);
+    expect(y[1]).to.be(2);
+    expect(y[2]).to.be(3);
+    expect(y[3]).to.be(4);
   });
 });
