704fb7184dc516313d7f99a4159aba37651e9312
Yosuke ONOUE <onoue@likr-lab.com>
2015-03-26 01:01:14 +0900

    Add dgetrf, dswap, and idamax

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function LinalgModule(stdlib, foreign, heap) {
 
   var abs = stdlib.Math.abs,
       imul = stdlib.Math.imul,
+      uiarray = new stdlib.Uint32Array(heap),
       darray = new stdlib.Float64Array(heap);
 
   function daxpy(n, alpha, x, incX, y, incY) {
@@ -91,6 +92,55 @@ function LinalgModule(stdlib, foreign, heap) {
     return value;
   }
 
+  function dswap(n, x, incX, y, incY) {
+    n = n | 0;
+    x = x | 0;
+    incX = incX | 0;
+    y = y | 0;
+    incY = incY | 0;
+
+    var i = 0,
+        j = 0,
+        tmp = 0.0;
+
+    n = x + n << 3;
+    x = x << 3;
+    incX = incX << 3;
+    y = y << 3;
+    incY = incY << 3;
+
+    for (i = x, j = y; (i | 0) < (n | 0); i = i + incX | 0, j = j + incY | 0) {
+      tmp = +darray[i >> 3];
+      darray[i >> 3] = darray[j >> 3];
+      darray[j >> 3] = tmp;
+    }
+  }
+
+  function idamax(n, x, incX) {
+    n = n | 0;
+    x = x | 0;
+    incX = incX | 0;
+
+    var i = 0,
+        index = 0,
+        tmp = 0.0,
+        value = 0.0;
+
+    n = (imul(n, incX) | 0) << 3;
+    x = x << 3;
+    incX = incX << 3;
+
+    for (i = x; (i | 0) < (n | 0); i = i + incX | 0) {
+      tmp = +abs(darray[i >> 3]);
+      if (tmp > value) {
+        index = (i - x | 0) / (incX | 0) |  0;
+        value = tmp;
+      }
+    }
+
+    return index | 0;
+  }
+
   function dgemm(transa, transb, m, n, k, alpha, a, lda, b, ldb, beta, c, ldc) {
     // TODO support transa, transb, lda, ldb, ldc
     transa = transa | 0;
@@ -127,12 +177,50 @@ function LinalgModule(stdlib, foreign, heap) {
     }
   }
 
+  function dgetrf(m, n, a, lda, ipiv) {
+    // TODO support lda
+    m = m | 0;
+    n = n | 0;
+    a = a | 0;
+    lda = lda | 0;
+    ipiv = ipiv | 0;
+
+    var i = 0,
+        j = 0,
+        k = 0,
+        l = 0,
+        jk = 0,
+        jl = 0,
+        kk = 0,
+        kl = 0;
+
+    for (k = 0; (k | 0) < (n - 1 | 0); k = k + 1 | 0) {
+      i = k + (idamax(n - k | 0, a + (imul(i, n) | 0) + k | 0, m) | 0) | 0;
+      uiarray[ipiv + (k << 2) >> 2] = i;
+      dswap(n, a + (imul(i, n) | 0) | 0, 1, a + (imul(k, n) | 0) | 0, 1);
+      for (j = k + 1 | 0; (j | 0) < (n | 0); j = j + 1 | 0) {
+        jk = a + (imul(j, n) | 0) + k << 3;
+        kk = a + (imul(k, n) | 0) + k << 3;
+        darray[jk >> 3] = darray[jk >> 3] / darray[kk >> 3];
+        for (l = k + 1 | 0; (l | 0) < (n | 0); l = l + 1 | 0) {
+          jl = a + (imul(j, n) | 0) + l << 3;
+          kl = a + (imul(k, n) | 0) + l << 3;
+          darray[jl >> 3] = darray[jl >> 3] - darray[jk >> 3] * darray[kl >> 3];
+        }
+      }
+    }
+    uiarray[ipiv + (n - 1 << 2) >> 2] = n - 1;
+  }
+
   return {
     daxpy: daxpy,
     dasum: dasum,
     dcopy: dcopy,
     ddot: ddot,
-    dgemm: dgemm
+    dswap: dswap,
+    idamax: idamax,
+    dgemm: dgemm,
+    dgetrf: dgetrf
   };
 }
 
--- test.js
+++ test.js
@@ -87,6 +87,94 @@ describe('ddot', function() {
   });
 });
 
+describe('dswap', function() {
+  it('calculates x, y := y, x', function() {
+    var heap = new ArrayBuffer(64),
+        x = new Float64Array(heap, 0, 4),
+        y = new Float64Array(heap, 32, 4),
+        linalg = LinalgModule(global, null, heap);
+
+    x[0] = 1;
+    x[1] = 2;
+    x[2] = 3;
+    x[3] = 4;
+    y[0] = 5;
+    y[1] = 6;
+    y[2] = 7;
+    y[3] = 8;
+
+    var value = linalg.dswap(4, 0, 1, 4, 1);
+
+    expect(x[0]).to.be(5);
+    expect(x[1]).to.be(6);
+    expect(x[2]).to.be(7);
+    expect(x[3]).to.be(8);
+    expect(y[0]).to.be(1);
+    expect(y[1]).to.be(2);
+    expect(y[2]).to.be(3);
+    expect(y[3]).to.be(4);
+  });
+
+  it('calculates x, y := y, x with offset', function() {
+    var heap = new ArrayBuffer(64),
+        x = new Float64Array(heap, 0, 4),
+        y = new Float64Array(heap, 32, 4),
+        linalg = LinalgModule(global, null, heap);
+
+    x[0] = 1;
+    x[1] = 2;
+    x[2] = 3;
+    x[3] = 4;
+    y[0] = 5;
+    y[1] = 6;
+    y[2] = 7;
+    y[3] = 8;
+
+    var value = linalg.dswap(3, 1, 1, 4, 1);
+
+    expect(x[0]).to.be(1);
+    expect(x[1]).to.be(5);
+    expect(x[2]).to.be(6);
+    expect(x[3]).to.be(7);
+    expect(y[0]).to.be(2);
+    expect(y[1]).to.be(3);
+    expect(y[2]).to.be(4);
+    expect(y[3]).to.be(8);
+  });
+});
+
+describe('idamax', function() {
+  it('returns index of max abs value', function() {
+    var heap = new ArrayBuffer(32),
+        x = new Float64Array(heap, 0, 4),
+        linalg = LinalgModule(global, null, heap);
+
+    x[0] = 0.5;
+    x[1] = -1.5;
+    x[2] = 2.5;
+    x[3] = -3.5;
+
+    var result = linalg.idamax(4, 0, 1);
+
+    expect(result).to.be(3);
+  });
+
+  it('returns index of max abs value with incX', function() {
+    var heap = new ArrayBuffer(32),
+        x = new Float64Array(heap, 0, 4),
+        linalg = LinalgModule(global, null, heap);
+
+    x[0] = 0.5;
+    x[1] = -1.5;
+    x[2] = 2.5;
+    x[3] = -3.5;
+
+    var result = linalg.idamax(2, 1, 2);
+
+    expect(result).to.be(1);
+  });
+});
+
 describe('dgemm', function() {
   it('calculate C := a A B + b C', function() {
     var heap = new ArrayBuffer(216),
@@ -134,5 +222,54 @@ describe('dgemm', function() {
     expect(c[6]).to.be( 210  );
     expect(c[7]).to.be(  31  );
     expect(c[8]).to.be(  47.5);
+  });
+});
+
+describe('dgetrf', function() {
+  it('computes the LU factorization of a real m by n matrix', function() {
+    var heap = new ArrayBuffer(144),
+        a = new Float64Array(heap, 0, 16),
+        ipiv = new Int32Array(heap, 128, 4),
+        linalg = LinalgModule(global, null, heap);
+
+    a[ 0] =  1.80;
+    a[ 1] =  2.88;
+    a[ 2] =  2.05;
+    a[ 3] = -0.89;
+    a[ 4] =  5.25;
+    a[ 5] = -2.95;
+    a[ 6] = -0.95;
+    a[ 7] = -3.80;
+    a[ 8] =  1.58;
+    a[ 9] = -2.69;
+    a[10] = -2.90;
+    a[11] = -1.04;
+    a[12] = -1.11;
+    a[13] = -0.66;
+    a[14] = -0.59;
+    a[15] =  0.80;
+
+    linalg.dgetrf(4, 4, 0, 4, 128);
+
+    expect(a[ 0]).to.be( 5.25                );
+    expect(a[ 1]).to.be(-2.95                );
+    expect(a[ 2]).to.be(-0.95                );
+    expect(a[ 3]).to.be(-3.8                 );
+    expect(a[ 4]).to.be( 0.34285714285714286 );
+    expect(a[ 5]).to.be( 3.8914285714285715  );
+    expect(a[ 6]).to.be( 2.3757142857142854  );
+    expect(a[ 7]).to.be( 0.4128571428571427  );
+    expect(a[ 8]).to.be( 0.30095238095238097 );
+    expect(a[ 9]).to.be(-0.46311796377875664 );
+    expect(a[10]).to.be(-1.5138592755751348  );
+    expect(a[11]).to.be( 0.29482060695056267 );
+    expect(a[12]).to.be(-0.21142857142857144 );
+    expect(a[13]).to.be(-0.3298825256975037  );
+    expect(a[14]).to.be( 0.004723367663983699);
+    expect(a[15]).to.be( 0.13137323948785168 );
+    expect(ipiv[0]).to.be(1);
+    expect(ipiv[1]).to.be(1);
+    expect(ipiv[2]).to.be(2);
+    expect(ipiv[3]).to.be(3);
   });
 });
